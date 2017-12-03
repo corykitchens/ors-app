198de200f3ab984eeff241be089fc1e26e1cef87
Cory Kitchens <corykitchens@gmail.com>
2017-12-03 12:11:32 -0800

    Remove process.exit call and use sigquit for the time being

--- test/ors.test.js
+++ test/ors.test.js
@@ -2,25 +2,38 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
 const Course = require('../api/models/Course.Model.js');
-const OnlineRegistrationSystem = require('ors-cp');
+const OnlineRegistration = require('ors-cp');
 
 
-describe('OnlineRegistrationSystem usage', () => {
+const expectedData = {
+  totalReservedSeats: 14,
+  enabledReserevedSeats: 12,
+  reservedSeatsAvailable: 9,
+  totalOpenSeats: 18,
+  openSeatsAvailable: 17
+};
 
-  var ors;
-  var c;
+// const osr = new OnlineRegistration(classData);
 
-  before((done) => {
-    Course.findOne({}, (err, course) => {
-      if (err) throw err;
-      ors = new OnlineRegistrationSystem(course);
-      done();
-    })
+describe('OnlineRegistration usage', () => {
+  it('should be usable with an instance of a Course', function(done) {
+    // 
+    Course.findOne({name: 'PHIL 316 - Professional Ethics'}, (err, course) => {
+      if (err) {
+        throw err;
+        done();
+      } else {
+        const ors = new OnlineRegistration(course);
+        expect(ors.getTotalReservedSeats()).to.equal(expectedData.totalReservedSeats);
+        expect(ors.getEnabledReservedSeats()).to.equal(expectedData.enabledReserevedSeats);
+        expect(ors.getReservedSeatsAvailable()).to.equal(expectedData.reservedSeatsAvailable);
+        expect(ors.getTotalOpenSeats()).to.equal(expectedData.totalOpenSeats);
+        expect(ors.getOpenSeatsAvailable()).to.equal(expectedData.openSeatsAvailable);
+        const obj = ors.showCourseEnrollment();
+        expect(obj.openSeatsAvailable).to.equal(expectedData.openSeatsAvailable);
+        expect(obj.reservedSeatsAvailable).to.equal(expectedData.reservedSeatsAvailable);
+        done();
+      }
+    });
   });
-
-  it('should have specific instance properties', () => {
-    expect(ors.classData).to.not.be.null;
-  });
-
-  
-})
+});
