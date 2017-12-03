a26672834606fce569e2550abdb9f839d5038d27
Cory Kitchens <corykitchens@gmail.com>
2017-12-02 17:35:06 -0800

    Basic crud operations on Course model

--- api/controllers/Course.Controller.js
+++ api/controllers/Course.Controller.js
@@ -3,22 +3,55 @@ const Course = require('../models/Course.Model.js');
 
 
 module.exports.getAllCourses = (req, res) => {
-  
-  res.status(200).json({message: "Get All Courses"});
+  Course.find((err, courses) => {
+    if (err) return responseWithError(err, res);
+    return responseWithSuccess(res, courses);
+  });
 }
 
 module.exports.getCourse = (req, res) => {
-  res.status(200).json({message: "Get All Courses"});
+  Course.findOne({
+    _id: req.params.courseId
+  }, (err, course) => {
+    if (err) return responseWithError(err, res);
+    return responseWithSuccess(res, course);
+  });
 }
 
 module.exports.createCourse = (req, res) => {
-  res.status(200).json({message: "Create a New Course"});
+  const course = new Course();
+  course.save((err) => {
+    if (err) return responseWithError(err, res);
+    return responseWithSuccess(res, course);
+  })
 }
 
 module.exports.updateCourse = (req, res) => {
-  res.status(200).json({message: "Get All Courses"});
+  Course.findOne({
+    _id: req.params.courseId
+  }, (err, course) => {
+    if (err) return responseWithError(err, res);
+    course = req.body;
+    // Call save?
+    return responseWithSuccess(res, course);
+  });
 }
 
 module.exports.deleteCourse = (req, res) => {
-  res.status(200).json({message: "Get All Courses"});
+  Course.remove({
+    _id: req.params.id,
+  }, (err) => {
+    if (err) return responseWithError(err, res);
+    return responseWithSuccess(res, {});
+  });
+
+}
+
+
+function responseWithError(err, res) {
+  return res.status(500).send(err)
+}
+
+function responseWithSuccess(res, data) {
+  return res.status(200).json(data);
 }
