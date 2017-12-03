105d39a26ec81c1c7e5d0b34bb58440c7bfae2c2
Cory Kitchens <corykitchens@gmail.com>
2017-12-02 18:37:26 -0800

    Tests passing green

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,13 +1,29 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
+const Course = require('../api/models/Course.Model.js');
+
 
 chai.use(chaiHttp);
 
 const server = require('../server.js');
 
 describe('ORS-App API Endpoints', () => {
-  //GET '/api/
+  const host = 'http://localhost';
+  const port = process.env.PORT || 3000;
+  var url = `${host}/${port}`;
+
+  var course;
+  
+
+  before(() => {
+    course = new Course();
+    course.save((err) => {
+      if (err) throw err;
+      url = '/api/courses/' + course._id;
+    })
+  });
+
   it('accept a GET request at landing page', (done) => {
     chai.request('http://localhost:3000')
       .get('/api')
@@ -15,7 +31,6 @@ describe('ORS-App API Endpoints', () => {
          expect(err).to.be.null;
          expect(res).to.have.status(200);
          expect(res).to.be.json;
-         expect(res.body).to.have.keys('message');
          done();
       });
   });
@@ -27,55 +42,51 @@ describe('ORS-App API Endpoints', () => {
          expect(err).to.be.null;
          expect(res).to.have.status(200);
          expect(res).to.be.json;
-         expect(res.body).to.have.keys('message');
          done();
       });
   });
   //GET /api/courses/:courseId
   it('accept a GET request and params for a specific course', (done) => {
     chai.request('http://localhost:3000')
-      .get('/api/courses/cmps211')
+      .get(url)
       .end(function (err, res) {
          expect(err).to.be.null;
          expect(res).to.have.status(200);
          expect(res).to.be.json;
-         expect(res.body).to.have.keys('message');
          done();
       });
   });
   //PUT /api/courses/:courseId
   it('accept a PUT request and params for a specific course', (done) => {
     chai.request('http://localhost:3000')
-      .put('/api/courses/cmps211')
+      .put(url)
       .end(function (err, res) {
+        console.log(err);
          expect(err).to.be.null;
          expect(res).to.have.status(200);
          expect(res).to.be.json;
-         expect(res.body).to.have.keys('message');
          done();
       });
   });
   //DELETE /api/courses/:courseId
   it('accept a PUT request and params for a specific course', (done) => {
     chai.request('http://localhost:3000')
-      .delete('/api/courses/cmps211')
+      .delete(url)
       .end(function (err, res) {
          expect(err).to.be.null;
          expect(res).to.have.status(200);
          expect(res).to.be.json;
-         expect(res.body).to.have.keys('message');
          done();
       });
   });
   //GET /api/fsdfkm3e
   it('redirect a user for misguided endpoints', (done) => {
     chai.request('http://localhost:3000')
-      .delete('/api/asdkmnj23123')
+      .get('/api/asdasdasd')
       .end(function (err, res) {
          expect(err).to.not.be.null;
          expect(res).to.have.status(404);
          expect(res).to.be.json;
-         expect(res.body).to.have.keys('message');
          done();
       });
   });
