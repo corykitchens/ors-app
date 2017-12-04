44aad44bf51fdce55c4e239f2f59e7bcc337744d
Cory Kitchens <corykitchens@gmail.com>
2017-12-03 20:02:12 -0800

    Remove morgan import for production

--- config/express.config.js
+++ config/express.config.js
@@ -8,8 +8,6 @@ module.exports = function(server) {
   server.use(express.static('client'));
 
   server.set('PORT', process.env.PORT || 3000);
-  
-  server.use(morgan('combined'));
   // parse x-www-form-urlencoded data
   server.use(bodyParser.urlencoded({ extended: false }));
   // parse json
